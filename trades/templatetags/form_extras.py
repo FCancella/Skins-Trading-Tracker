b235126f6d8999bb8b47574bfa7b89c8782972a8
Cancella <cancellacdc@gmail.com>
2025-08-16 04:48:02 -0300

    second

---
+++ trades/templatetags/form_extras.py
@@ -0,0 +1,7 @@
+# trades/templatetags/form_extras.py
+from django import template
+register = template.Library()
+
+@register.filter
+def add_class(field, css):
+    return field.as_widget(attrs={**(field.field.widget.attrs or {}), "class": css})
