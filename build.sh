94e157484f076a337fe7da365447a3a834d172b5
FCancella <cancellacdc@gmail.com>
2025-08-19 07:54:55 -0300

    2 Try fix render deploy

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Set Django settings module and DEBUG to False for production static file collection
-export DJANGO_SETTINGS_MODULE="cs_trade_portfolio.settings"
-export DEBUG="False"
-
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 
 python manage.py migrate
--- cs_trade_portfolio/settings.py
+++ cs_trade_portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from __future__ import annotations
 
+import os
 import dj_database_url 
 from pathlib import Path
 
@@ -117,13 +118,13 @@ USE_TZ: bool = True
 STATIC_URL: str = '/static/'
 STATICFILES_DIRS: list[Path] = [BASE_DIR / 'static']
 
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#if not DEBUG:
+# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
