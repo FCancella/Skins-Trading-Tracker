efbcec09a3e711525d64b9552eed34bcca7a880d
FCancella <cancellacdc@gmail.com>
2025-08-19 07:50:39 -0300

    Try fix render deploy

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Set DEBUG to false for production static file collection
-export DJANGO_SETTINGS_MODULE=cs_trade_portfolio.settings
-export DEBUG=False
+# Set Django settings module and DEBUG to False for production static file collection
+export DJANGO_SETTINGS_MODULE="cs_trade_portfolio.settings"
+export DEBUG="False"
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 python manage.py migrate
