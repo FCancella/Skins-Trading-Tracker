882fcfc894c63d2ad3c352fc4cc19281b7a844c8
Cancella <cancellacdc@gmail.com>
2025-11-05 16:43:02 -0300

    Include on admin

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ScannedItem, BlackList, SchedulerLogs
+from .models import ScannedItem, BlackList, SchedulerLogs, Item, Collection, Crate
 
 @admin.register(ScannedItem)
 class ScannedItemAdmin(admin.ModelAdmin):
@@ -20,3 +20,24 @@ class SchedulerLogsAdmin(admin.ModelAdmin):
     list_display = ('timestamp', 'message')
     list_filter = ('timestamp',)
     search_fields = ('message',)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    """Admin view for Items."""
+    list_display = ('name', 'price', 'offers', 'price_time', 'rarity', 'category', 'special')
+    search_fields = ('name', 'market_hash_name')
+    list_filter = ('category', 'rarity', 'special', 'price_time')
+    # Deixa os campos M2M como apenas leitura no admin para evitar erros
+    readonly_fields = ('collections', 'crates') 
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    """Admin view for Collections."""
+    list_display = ('name', 'id')
+    search_fields = ('name', 'id')
+
+@admin.register(Crate)
+class CrateAdmin(admin.ModelAdmin):
+    """Admin view for Crates."""
+    list_display = ('name', 'id')
+    search_fields = ('name', 'id')
