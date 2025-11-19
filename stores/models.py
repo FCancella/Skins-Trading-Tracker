7ac35c88c5bba4122632aff7517210e424163c4a
Cancella <cancellacdc@gmail.com>
2025-11-19 07:42:54 -0300

    Update models.py

--- stores/models.py
+++ stores/models.py
@@ -14,7 +14,7 @@ class Store(models.Model):
     whatsapp = models.CharField(max_length=20, help_text="Número com DDD para contato", blank=True, null=True)
     
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} ({self.user.username})"
 
 class StoreItem(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='items')
